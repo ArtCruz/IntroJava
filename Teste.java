e90f9d2f18e42dd0b22026cb9f175b210467994d
Arthur Cruz <73409867+ArtCruz@users.noreply.github.com>
2022-07-20 19:56:19 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,32 @@
+
+public class Teste {
+	
+	public static void main(String[]  args) {
+		// Entrada
+		String nome = "Carlos";
+		int valor1 = 6;
+		int valor2 = 5;
+		
+		// Processamento 
+		int soma = valor1+valor2;
+		
+		// Saída		
+		System.out.println("Resultado: " + soma);
+		System.out.println("Nome: " + nome);
+		
+		//Valor inteiro
+		int valor = 10;
+		
+		//Valor com casas de precisão
+		double valor4 = 3.56;
+		float valor3 = 4.5f;
+		
+		//Valores alfanúmericos
+		String valor5 = "sdjfbiuadbias ";
+		char valor6 = '1';
+		
+		//Valores booleanos
+		boolean xpto = true;
+	}
+
+}
